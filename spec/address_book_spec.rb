fa3ea942abf8c28f4f0ec8328a4028e93b939999
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2015-11-22 13:54:00 -0500

    assignment 21 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -81,4 +81,33 @@ RSpec.describe AddressBook do
     end
 
   end
+
+  describe "#import_from_csv_2" do
+    it "imports the correct number of entries" do
+
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+
+      expect(book_size).to eq 3
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "George", "555-546-9852", "george@washington.com")
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Abraham", "555-263-5412", "abraham@lincoln.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Franklin", "555-632-4456", "franklin@roosevelt.com")
+    end
+
+  end
 end
